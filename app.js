5c62e8838559aeeec8a84f1b63247cb0150779a6
Diego <diegoteixeirapereira@hotmail.com>
2021-10-01 14:36:20 -0300

    adicionados arquivos de configuração do projeto.

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection = require('./lib/dataBase');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var livrosRouter = require('./routes/livros');
 
 var app = express();
 
@@ -19,23 +25,34 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+    cookie: { maxAge: 60000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
+
+app.use(flash());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/livros', livrosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
